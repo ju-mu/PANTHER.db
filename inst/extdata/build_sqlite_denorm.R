762f4a452d1eede8498f01d66e6b6ed38fa4ab63
Julius Muller <ju-mu@alumni.ethz.ch>
2013-08-31 03:02:04 +0200

    Update build_sqlite_denorm.R

--- inst/extdata/build_sqlite_denorm.R
+++ inst/extdata/build_sqlite_denorm.R
@@ -1,4 +1,3 @@
-
 #### Functions to build the sqlite DB for Package PANTHER.db ####
 # Generated for PANTHER version 8.2
 # All source files from the ftp at ftp://ftp.pantherdb.org//
@@ -9,6 +8,7 @@ library(RSQLite)
 
 #### SET THE TARGET FOLDER FOR ALL FILES FROM PANTHER USED FOR db BUILDING ####
 home_folder<-"~/Downloads/"
+source("scheme.txt")
 ### ###
 
 #### RESTRICT db TO SPECIES SUPPORTED BY AnnotationDb ####
