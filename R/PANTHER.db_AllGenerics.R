c887134c6e99ab08e2d43214daec5ece1768e43c
Julius Muller <ju-mu@alumni.ethz.ch>
2013-08-31 02:55:57 +0200

    Create PANTHER.db_AllGenerics.R

---
+++ R/PANTHER.db_AllGenerics.R
@@ -0,0 +1,4 @@
+
+setGeneric(".type2table", function(x) standardGeneric(".type2table"))
+setGeneric(".type2col", function(x) standardGeneric(".type2col"))
+setGeneric("getTableNames", function(x) standardGeneric("getTableNames"))
