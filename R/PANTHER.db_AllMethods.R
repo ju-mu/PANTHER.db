5872c18955a994686ff96435cb49608df098c85d
Julius Muller <ju-mu@alumni.ethz.ch>
2013-09-11 23:47:44 +0200

    Update PANTHER.db_AllMethods.R

--- R/PANTHER.db_AllMethods.R
+++ R/PANTHER.db_AllMethods.R
@@ -1,34 +1,41 @@
-
+
 .species <- function(x){
   x$.species
 } 
 setMethod("species", "PANTHER", .species)
 
 .cols <- function(x){
-  c("FAMILY_ID","GOSLIM_ID","GOSLIM_TERM","UNIPROT","SPECIES","FAMILY_TERM","SUBFAMILY_TERM","CLASS_ID","CLASS_TERM","PATHWAY_ID","PATHWAY_TERM","COMPONENT_ID","COMPONENT_TERM","EVIDENCE","EVIDENCE_TYPE","CONFIDENCE_CODE")
+  c("FAMILY_ID","GOSLIM_ID","GOSLIM_TERM","UNIPROT","SPECIES","FAMILY_TERM","SUBFAMILY_TERM","CLASS_ID","CLASS_TERM","PATHWAY_ID","PATHWAY_TERM","COMPONENT_ID","COMPONENT_TERM","EVIDENCE","EVIDENCE_TYPE","CONFIDENCE_CODE","ENTREZ")
 } 
 setMethod("columns", "PANTHER", .cols)
 
 
 
 type2table <- function(x){
-  cls<-c(x$.ref_table,"go_slim","go_slim","uniprot","uniprot",x$.ref_table,x$.ref_table,"protein_class","protein_class","panther_go","panther_go",rep("panther_go_component",5))
+  cls<-c(x$.ref_table,"go_slim","go_slim","uniprot","uniprot",x$.ref_table,x$.ref_table,"protein_class","protein_class","panther_go","panther_go",rep("panther_go_component",5),"entrez")
   names(cls)<-columns(x)
   cls
 } 
 setMethod(".type2table", "PANTHER", type2table)
 
+.availablePantherSpecies <- function(x)
+{
+  res<-dbGetQuery(x$conn, "SELECT * from species")
+  colnames(res)<-c("PANTHER Species","UNIPROT Species","UNIPROT Taxon ID")
+  res
+}
+setMethod("availablePantherSpecies", "PANTHER", .availablePantherSpecies)
 
 
 type2col <- function(x){
-  cls<-c("family_id","goslim_id","ontology","uniprot_id","species","family_term","subfamily_term","class_id","class_term","go_id","go_term","component_go_id","component_go_term","evidence","evidence_type","confidence_code")
+  cls<-c("family_id","goslim_id","ontology","uniprot_id","species","family_term","subfamily_term","class_id","class_term","go_id","go_term","component_go_id","component_go_term","evidence","evidence_type","confidence_code","entrez_id")
   names(cls)<-columns(x)
   cls
 } 
 setMethod(".type2col", "PANTHER", type2col)
 
 .keytypes <- function(x){
-  c("FAMILY_ID","GOSLIM_ID","CLASS_ID","PATHWAY_ID","COMPONENT_ID","UNIPROT","SPECIES")
+  c("FAMILY_ID","GOSLIM_ID","CLASS_ID","PATHWAY_ID","COMPONENT_ID","UNIPROT","SPECIES","ENTREZ")
 } 
 setMethod("keytypes", "PANTHER", .keytypes)
 
