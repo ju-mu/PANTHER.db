b66e59b96c26108069f4b5ee05878bfe5937fd76
Julius Muller <ju-mu@alumni.ethz.ch>
2013-09-17 23:34:14 +0200

    Update PANTHER.db_AllGenerics.R

--- R/PANTHER.db_AllGenerics.R
+++ R/PANTHER.db_AllGenerics.R
@@ -1,6 +1,11 @@
 
+setGeneric(".keytypes_core", function(x) standardGeneric(".keytypes_core"))
 setGeneric(".type2table", function(x) standardGeneric(".type2table"))
 setGeneric(".type2col", function(x) standardGeneric(".type2col"))
 setGeneric("getTableNames", function(x) standardGeneric("getTableNames"))
 setGeneric("availablePantherSpecies", function(x) standardGeneric("availablePantherSpecies"))
+setGeneric("resetSpecies", function(x) standardGeneric("resetSpecies"))
+setGeneric("species<-", function(x, value) standardGeneric("species<-"))
+setGeneric("traverseClassTree", function(x,query,scope) standardGeneric("traverseClassTree"))
+
 
