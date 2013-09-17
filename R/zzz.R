e8293a1223cf61dc44b217f8a5eaa59ec8c7d68c
Julius Muller <ju-mu@alumni.ethz.ch>
2013-09-17 23:34:46 +0200

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -2,10 +2,10 @@
 
 datacache <- new.env(hash=TRUE, parent=emptyenv())
 
-PANTHER <- function() showQCData("PANTHER", datacache)
+PANTHER <- function() showQCData("PANTHER.db", datacache)
 PANTHER_dbconn <- function() dbconn(datacache)
 PANTHER_dbfile <- function() dbfile(datacache)
-PANTHER_dbschema <- function(file="", show.indices=FALSE) dbschema(datacache, file=file, show.indices=show.indices)
+PANTHER_dbschema <- function() cat(readLines(system.file("extdata/PANTHER_DB.sql",package="PANTHER.db")), sep = "\n")
 PANTHER_dbInfo <- function() dbInfo(datacache)
 
 loadDb2<-function (file, dbType, dbPackage, ...) 
@@ -18,25 +18,28 @@ loadDb2<-function (file, dbType, dbPackage, ...)
 
 .onLoad <- function(libname, pkgname)
 {
-    
-    sPkgname <- sub(".db$","",pkgname)
+
     dbfile<-system.file("extdata", paste(pkgname,".sqlite",sep=""), package=pkgname, lib.loc=libname,mustWork=T)
     assign("dbfile", dbfile, envir=datacache)
     dbconn <- dbFileConnect(dbfile)
     assign("dbconn", dbconn, envir=datacache)
-    
-    
-    db <- loadDb2(file=dbfile,dbType=sPkgname,dbPackage=pkgname)
+      
+    db <- loadDb2(file=dbfile,dbType=pkgname,dbPackage=pkgname)
 
-    dbNewname<-AnnotationDbi:::dbObjectName(pkgname,sPkgname)
+    dbNewname<-AnnotationDbi:::dbObjectName(pkgname,pkgname)
     ns <- asNamespace(pkgname)
     assign(dbNewname, db, envir=ns)
     namespaceExport(ns, dbNewname)
     db$.setAvailableSpecies()
+	
+	packageStartupMessage("PANTHER.db version ", packageDescription(pkgname, fields="Version"))
+	
 }
 
 .onUnload <- function(libpath)
 {
 
 }
-getMethod("loadDb",c("character","character","character"))
+
+
+
